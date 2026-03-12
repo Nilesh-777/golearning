8178d1b3ed12b73fb71da3ed6b40f256b4d0aa2d
Nilesh Dahiphale <ndahiphale@mvement.co>
2026-03-12 21:42:34 +0530

    Splitting slices

--- TypeDefination/main.go
+++ TypeDefination/main.go
@@ -1,6 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
+	fmt.Println("Inital Deck")
+
+	// Print all cards
 	cards.print()
+
+	// Split & Print
+	hand, remainingCards := deal(cards, 2)
+	fmt.Println("Deck1: ")
+	hand.print()
+	fmt.Println("Deck2: ")
+	remainingCards.print()
 }
